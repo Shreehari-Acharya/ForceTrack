bf48a09e82f83d8cbbe7a2e64fc01e7fa4120e0d
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2025-06-16 18:53:54 +0530

    Add routing capability using browserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
-
+import { ThemeProvider } from "components/theme-provider"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Dashboard from "pages/dashboard"
+import StudentDetails from "pages/studentDetails"
+import Navbar from "components/navbar"
 
 function App() {
 
   return (
-    <>
-      <div className="text-7xl text-cyan-500">Hello world</div>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <div className="min-h-screen">
+          <Navbar/>
+          <main className="mt-8">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/student/:id" element={<StudentDetails />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
