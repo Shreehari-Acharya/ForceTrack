fb47be5f897b625ac2694d321099a6430f1f0704
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2025-06-18 00:41:57 +0530

    Add settings to navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,10 +1,18 @@
 import ThemeToggle from "./theme-toggle"
+import { Button } from "./ui/button"
+import { FiSettings } from "react-icons/fi"
 
 export default function Navbar() {
     return (
         <nav className="flex justify-between items-center p-4 shadow shadow-bottom shadow-foreground/15 shadow-md/90">
             <div className="text-3xl font-semibold">TLE - Analytics</div>
-            <ThemeToggle />
+            <div className="items-center flex space-x-2">
+                <ThemeToggle />
+                <Button variant={"outline"} className={"flex items-center"}>
+                    <FiSettings className="h-3 w-3" />
+                    Settings
+                </Button>
+            </div>
         </nav>
     )
 }
